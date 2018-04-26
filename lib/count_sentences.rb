74f30e384825822226a81982cf6ab177c8d2205a
Derek Chen <derek.g.chen@gmail.com>
2018-04-26 01:35:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if (self.end_with?("."))
+    false
   end
 
   def question?
+    return true if (self.end_with?("?"))
+    false
 
   end
 
   def exclamation?
+    return true if (self.end_with?("!"))
+    false
 
   end
 
   def count_sentences
+    temp= self.squeeze
+    n = temp.count(".")
+    n +=temp.count("?")
+    n +=temp.count("!")
 
   end
 end
